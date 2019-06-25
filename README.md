31f7d2d406b3abefb779970c8ae7d36a45a0e8bb
Mitch Olson <mitchjolson@gmail.com>
2019-06-25 13:16:36 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Intro to Node
+
+Node interprets javascript and runs the cude without a browser. No HTML needed.
+
+
+npm - node package manager
+
+
+npm init -y   (the -y will accept all defaults)
+npm install (packagename)
+node (js file name)
+nmp start - runs "start" section in package.json
+
+.json stands for JavaScript Object Notation
+It's like an about file for your javascript, contains info and dependencies
---
+++ index.js
@@ -0,0 +1,22 @@
+const characters = [
+    {
+        name: "Spike",
+        show: "Cowboy Bebop",
+    },
+    {
+        name: "L",
+        show: "Death Note"
+    },
+    {
+        name: "Goku",
+        show: "Dragonball Z"
+    },
+    {
+        name: "Gon",
+        show: "Hunter X Hunter"
+    }
+]
+
+for( character of characters ){
+    console.log(`${character.name} is a character in the show ${character.show}.`);
+}
